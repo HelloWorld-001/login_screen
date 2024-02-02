d012bae57171178569c808d4f4e9bb04bc1fa381
HelloWorld-001 <hw01011000@gmail.com>
2024-02-02 09:40:30 +0530

    SignUp form Ui

--- lib/main.dart
+++ lib/main.dart
@@ -22,8 +22,16 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +52,7 @@ class LoginPage extends StatelessWidget {
                 height: media.height, width: media.width,
                 margin: EdgeInsets.symmetric(horizontal: media.width/10, vertical: media.height/7),
                 child: ClipPath(
-                  clipper: MyClipper(
+                  clipper: CardClipper(
                     innerRadius: innerRadius, outerRadius: outerRadius,
                     size: Size(media.width/1.25, media.height/1.4)
                   ),
@@ -52,7 +60,95 @@ class LoginPage extends StatelessWidget {
                     elevation: 2,
                     shadowColor: Colors.white.withOpacity(0.9),
                     color: Colors.white.withOpacity(0.8),
-                    child: SignUpForm(),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20, right: 20, top: 75),
+                      child: Column(
+                        // mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Signup",
+                            style: TextStyle(color: Colors.black, fontSize: 40, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 15),
+                          TF(
+                            controller: usernameController,
+                            textInputType: TextInputType.name,
+                            hintText: "Username",
+                            prefixIcon: Icons.person,
+                          ),
+                          SizedBox(height: 20),
+                          TF(
+                            controller: passwordController,
+                            textInputType: TextInputType.text,
+                            hintText: "Email",
+                            prefixIcon: Icons.email_rounded,
+                          ),
+                          SizedBox(height: 20),
+                          TF(
+                            controller: passwordController,
+                            textInputType: TextInputType.text,
+                            hintText: "Password",
+                            prefixIcon: Icons.password,
+                          ),
+                          SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ButtonStyle(
+                              fixedSize: MaterialStatePropertyAll(Size(double.maxFinite, 50)),
+                              backgroundColor: MaterialStatePropertyAll(Colors.black),
+                              overlayColor: MaterialStatePropertyAll(Colors.grey.shade700),
+                              padding: MaterialStatePropertyAll(EdgeInsets.symmetric(vertical: 10))
+                            ),
+                            child: Text(
+                              "SignUp",
+                              style: TextStyle(color: Colors.lightGreenAccent, fontSize: 25),
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          SizedBox(
+                            width: double.maxFinite, height: 20,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Expanded(child: Container(color: Colors.grey.shade400, height: 1)),
+                                SizedBox(width: 10),
+                                Text(
+                                  "OR",
+                                  style: TextStyle(fontSize: 20, color: Colors.grey.shade400, fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(width: 10),
+                                Expanded(child: Container(color: Colors.grey.shade400, height: 1)),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ButtonStyle(
+                              shape: MaterialStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(12))),
+                              backgroundColor: MaterialStatePropertyAll(Colors.black),
+                              padding: MaterialStatePropertyAll(EdgeInsets.symmetric(vertical: 5)),
+                              overlayColor: MaterialStatePropertyAll(Colors.grey.shade700)
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SizedBox(
+                                  width: 40, height: 40,
+                                  child: Image.asset("assets/images/google.png", scale: 0.1)
+                                ),
+                                SizedBox(width: 10),
+                                Text(
+                                  "Sign Up with Google",
+                                  style: TextStyle(color: Colors.white, fontSize: 20),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
                   ),
                 ),
               ),
@@ -71,11 +167,11 @@ class LoginPage extends StatelessWidget {
   }
 }
 
-class MyClipper extends CustomClipper<Path> {
+class CardClipper extends CustomClipper<Path> {
   double outerRadius = 0;
   double innerRadius = 0;
   Size size = Size(0, 0);
-  MyClipper({required this.outerRadius, required this.innerRadius, required this.size});
+  CardClipper({required this.outerRadius, required this.innerRadius, required this.size});
   @override
   Path getClip(Size size) {
     size = this.size;
@@ -97,28 +193,60 @@ class MyClipper extends CustomClipper<Path> {
 }
 
 
-class SignUpForm extends StatelessWidget {
-  SignUpForm({super.key});
+class TF extends StatefulWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final IconData prefixIcon;
+  final TextInputType textInputType;
+  const TF({super.key, required this.controller, required this.hintText, required this.prefixIcon, required this.textInputType});
 
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 70),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          TF()
-        ],
-      ),
-    );
-  }
+  State<TF> createState() => _TFState();
 }
 
-class TF extends StatelessWidget {
-  const TF({super.key});
+class _TFState extends State<TF> {
+  bool pass = false;
+  bool showPass = false;
+
+  @override
+  void initState() {
+    super.initState();
+    if(widget.hintText == "Password") {
+      setState(() {
+        pass = true;
+      });
+    }
+  }
+
+  void changeVisibility() {
+    setState(() {
+      showPass = (showPass == true) ? false : true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final border = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12),
+      borderSide: BorderSide(color: Colors.deepPurple, width: 2)
+    );
+    return TextField(
+      controller: widget.controller,
+      obscureText: showPass,
+      keyboardType: widget.textInputType,
+      style: TextStyle(color: Colors.black, fontSize: 20),
+      decoration: InputDecoration(
+        border: border,
+        enabledBorder: border,
+        hintText: "",
+        labelStyle: TextStyle(color: Colors.grey.shade800),
+        alignLabelWithHint: true,
+        labelText: widget.hintText,
+        prefixIcon: Icon(widget.prefixIcon, color: Colors.deepPurple, size: 20),
+        suffixIcon: (widget.hintText != "Password")
+        ? null
+        : IconButton(onPressed: changeVisibility, icon: Icon((showPass) ? Icons.visibility : Icons.visibility_off, color: Colors.grey)),
+      ),
+    );
   }
 }
